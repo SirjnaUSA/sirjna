214a394a2ce68d77dbe0d392662d814e922168df
Sirjna <contact@sirjna.com>
2025-10-30 14:26:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup, find_packages
-setup(name='sirjna', version='0.1.0', packages=find_packages(), include_package_data=True, zip_safe=False, install_requires=['requests>=2.31.0'])
+setup(name='sirjna', version='0.1.0', packages=find_packages())
