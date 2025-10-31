95e546185914cb4556e36b97db70484c3ce95e1d
SirjnaUSA <contact@sirjna.com>
2025-10-31 06:54:43 -0700

    delete

--- setup.py
+++
@@ -1,2 +0,0 @@
-from setuptools import setup, find_packages
-setup(name="sirjna", version="0.1.2", packages=find_packages(), include_package_data=True, zip_safe=False)
--- sirjna/sirjna/config/desktop.py
+++
@@ -1,9 +0,0 @@
-from frappe import _
-def get_data():
-    return [{
-        "module_name":"Sirjna",
-        "color":"black",
-        "icon":"octicon octicon-rocket",
-        "type":"module",
-        "label": _("Sirjna")
-    }]
